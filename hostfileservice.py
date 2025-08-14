8cb2b783551341a6406436ccaf8cb22df28cf4ef
Raul Lucas <raullinjetmc@gmail.com>
2025-08-14 18:46:14 -0500

    added os handling

--- hostfileservice.py
+++ hostfileservice.py
@@ -3,3 +3,20 @@ import time
 from datetime import datetime as DT
 import platform
 
+os_name = platform.system()
+print(f"The OS detected is: {os_name}")
+
+host_temp = "hosts.txt"
+ip = "127.0.0.1"
+
+if (os_name == "Windows"):
+    host_path = r"C:\Windows\System32\drivers\etc\hosts"
+    print("Welcome to Windows!")
+    
+elif (os_name == "Darwin"):
+    host_path = r"/etc/hosts"
+    print("Hello mac users")
+
+# add linux option here
+
+
