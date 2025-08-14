08a034529aa5f206e4271ae547fa5a65d650e567
Raul Lucas <raullinjetmc@gmail.com>
2025-08-14 16:01:36 -0500

    added start timer and unblock methods

--- core/timer.py
+++ core/timer.py
@@ -2,8 +2,21 @@ import timer
 import threading
 
 class Timer:
-    def __init__(self):
-        
-
-if __name__ == "__main__":
+    def __init__(self, durationinseconds):
+        self.durationinseconds = durationinseconds
+        self.timeremaining = durationinseconds
+        self.starttimer = None
+        self.timerrunning = None
+        self.timerthread = None
+        self.unblocktimer = None
     
+    def unblock(self):
+        
+    
+    def start(self):
+        print("timer is starting")
+        t = threading.Timer(30.0, self.unblock).start()
+
+"""
+if __name__ == "__main__":
+""" 
