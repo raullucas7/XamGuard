753dde90175cc548bd06c1bd8e2fbada23fa62e0
Raul Lucas <raullinjetmc@gmail.com>
2025-08-18 15:35:36 -0500

    changing input flow to a dictionary router

---
+++ main.py
@@ -0,0 +1,58 @@
+from threading import Timer
+
+INPUT_HANDLING = {
+    "site" : sitelogic,
+    "app" : applogic,
+    "unblock" : unblocklogic,
+    "quit" : quitlogic
+}
+
+
+def sitelogic():
+    return
+
+def applogic():
+    return
+
+def unblocklogic():
+    return
+
+def quitlogic():
+    return
+
+
+
+def main():
+    print("Welcome to XamGuard, your personal focus app.")
+    
+    
+    
+    while True:
+        optionchoice = input("Choose: site | app | unblock | quit")
+        
+        if (optionchoice == ("site").lower().strip()):
+            sitechoices = input("Enter websites (comma separated like 'youtube.com'): ")
+            
+            
+            
+            
+        
+        elif (optionchoice == ("app").lower().strip()):
+            appchoices = input("Enter the apps you wish to block")
+            
+        elif (optionchoice == ("unblock").lower().strip()):
+            unblockchoice = input("")
+        else:
+            print("Quitting")
+            exit()
+
+
+
+"""
+def siteunblock():
+    print("Unblocking sites immediately")
+    
+    timer = Timer(10, siteunblock)
+    timer.start()
+
+"""
