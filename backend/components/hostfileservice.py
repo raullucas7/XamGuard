86d23a4238e147ee509a525b15962c51a9755078
Raul Lucas <raullinjetmc@gmail.com>
2025-09-04 21:25:54 -0500

    fixed block function and added Linux support

--- backend/components/hostfileservice.py
+++ backend/components/hostfileservice.py
@@ -12,48 +12,44 @@ ip = "127.0.0.1"
 
 if (os_name == "Windows"):
     host_path = r"C:\Windows\System32\drivers\etc\hosts"
-    print("Welcome to Windows!")
+    print("Hello Windows user!")
     
 elif (os_name == "Darwin"):
     host_path = r"/etc/hosts"
-    print("Hello mac users")
+    print("Hello Mac user!")
 
-# add linux option here
-
+elif (os_name == "Linux"):
+    host_path = r"/etc/hosts"
+    print("Hello Linux user!")
 
 def block(websites):
     with open(host_path, "r+") as f:
         filetext = f.read()
     
-    linestoadd = []
-    
-    for i in websites:
-        site = i.strip()
+        linestoadd = []
         
-        if not site:
-            continue
-        
-        # exact ip and site 
-        if f"{ip} {site}" in filetext or f"{ip}\t{site}" in filetext:
-            print(f"{site} already blocked")
-        else:
-            linestoadd.append(f"{ip} {site} {TAGS}\n")
+        for i in websites:
+            site = i.strip()
             
-    if not linestoadd:
-        print("Nothing to add")
-        return
-    
-    with open(host_path, "w") as g:
-        g.write(linestoadd)
-    
-    print("blocked this site")
-
+            if not site:
+                continue
+            
+            # exact ip and site 
+            if f"{ip} {site}" in filetext or f"{ip}\t{site}" in filetext:
+                print(f"{site} already blocked")
+            else:
+                linestoadd.append(f"{ip} {site} {TAGS}\n")
+                
+        if not linestoadd:
+            print("Nothing to add")
+            return
+        
+        # stop writing over the sites; 0 = start, 2 = end
+        f.seek(0, 2)
+        
+        for line in linestoadd:
+            f.write(line)
+    print("Blocked the following sites:", websites)
 
 def unblock(websites):
-    # yes
-    
-    
-    
-    print("blocked this site")
-    
     return
