1226d7021161e81ad0bc559de45aece0fe5a3805
Raul Lucas <raullinjetmc@gmail.com>
2025-09-25 15:18:38 -0500

    added a POST endpoint for API

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,6 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from components.hostfileservice import block, unblock
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -6,7 +8,17 @@ app = Flask(__name__)
 def home():
     return "Hello Xamguard"
 
-@app.route("/", methods=["POST"])
+# GET : getting/sending info to a website/client
+#   - use if you dont care about sensitivity of info
+# POST : more secure GET 
+#   - use if info is sensitive (form data)
 
+@app.route("/api/block", methods=["POST"])
 def apiblock():
-    return
+    data = request.get_json()
+    sites = data.get("sites", [])
+    block(sites)
+    return jsonify({"status": "success", "blocked": sites})
+
+if __name__ == "__main__":
+    app.run(debug=True)
