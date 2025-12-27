e46ff458f08ea605629ec1427af9642638cd93b5
Raul Lucas <raullinjetmc@gmail.com>
2025-12-27 14:03:48 -0600

    added get and post methods

--- backend/app.py
+++ backend/app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from components.hostfileservice import block, unblock
 
 app = Flask(__name__)
-
+blocked_sites = []
 @app.route("/")
 
 def home():
@@ -19,6 +19,25 @@ def apiblock():
     sites = data.get("sites", [])
     block(sites)
     return jsonify({"status": "success", "blocked": sites})
+
+
+@app.route("/api/blocking", methods=["GET"])
+def getblockedsites():
+    return jsonify({"blocked_sites": blocked_sites})
+
+# unblock the site lil bro
+@app.route("/api/unblock", methods=["POST"])
+def apiblock():
+    data = request.get_json()
+    sites = data.get("sites", [])
+    unblock(sites)
+    
+    for site in sites:
+        if site in blocked_sites:
+            blocked_sites.remove(site)
+    
+    return jsonify({"status": "success", "unblocked": sites})
+
 
 if __name__ == "__main__":
     app.run(debug=True)
