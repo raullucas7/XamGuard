ac2f44c5758376c786105cb8ce8e89fc11dd3ddf
Raul Lucas <raullinjetmc@gmail.com>
2026-01-28 14:53:19 -0600

    completed vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,3 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 
+// code used from confid page on Vite -> https://vite.dev/config/
+// if needed, just do /api/block instead of above link
+export default defineConfig({
+    plugins: [react()],
+    server: {
+        proxy: {
+            '/api': {
+                target: 'http://127.0.0.1:5000',
+                changeOrigin: true,
+                secure: false,
+            },
+        },
+    },
+})
