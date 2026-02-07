b76df90ce4898e80b04acb9bdf3743207efa2dbb
Raul Lucas <raullinjetmc@gmail.com>
2026-02-06 19:33:39 -0600

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+
+// IMPORTANT NOTE: if site restarts, does blockedsites empty? 
+// EDGE CASE: does the actual hosts file on my computer stay blocked while the app 'forgets' they are blocked?
+
+function App() {
+
+}
+
+export default App
