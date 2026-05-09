bda2f62b4bd86e9780a8dec1465174e8feb4ab40
Raul Lucas <raullinjetmc@gmail.com>
2026-05-08 21:27:55 -0500

    fixed timer

--- backend/components/timer.py
+++ backend/components/timer.py
@@ -1,19 +1,40 @@
-from threading import Timer
+# timer.py
+from threading import Timer as threadingTimer
 
 class Timer:
-    def __init__(self, durationinseconds, unblocktimer):
+    def __init__(self, durationinseconds, timerend):
         self.duration = durationinseconds
-        self.unblocktimer = durationinseconds
+        self.timerend = timerend
+        self.running = None
         self.timer = None
     
     def start(self):
+        if self.running:
+            print("Timer is running.")
+            return
+        
         print("Timer running for {self.durationinseconds} seconds")
-        self.timer = Timer(self.durationinseconds, self.unblocktimer)
+        self.timer = threadingTimer(self.durationinseconds, self.handletimer)
         self.timer.start()
         my_timer = 5
-        
+    
+    def handletimer(self):
+        self.running = False
+        print("The timer is done.")
+        if self.timerend:
+            self.timerend
+
+
+    def ongoing(self):
+        if self.running:
+            print("The timer is ongoing for {self.durationinseconds}.")
+        else:
+            print("Timer is not running. Create a new instance.")
     
     def abort(self):
-        if self.timer:
+        if self.timer and self.running:
             self.timer.cancel()
+            self.running = False
             print("Timer cancelled")
+        else:
+            print("No active timer to abort.")
